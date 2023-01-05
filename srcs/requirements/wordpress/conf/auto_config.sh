5355eb6a1fd28fe1ba08c06d4fe251107118d412
louisea <louiseamelie2@gmail.com>
2023-01-05 17:59:05 +0100

    mariadb working wordpress I hate u

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,20 +1,20 @@
-# script qui sert a effectuer les commandes au lancement du container WP
+# script qui sert a effectuer les commandes au lancement du container WP (pour une configuration automatique cf sujet)
 
 sleep 10 
 	# on veut etre sur que MariaDB c'est lance correctement avant de commencer la config WP
 
-if [ ! -f wp-config.php ];
+if [ -f /var/www/wordpress/wp-config.php ];
 then
-	wp config create --allow-root \
-		--dbname=$SQL_DATABASE \ #set the database name
-		--dbuser=$SQL_USER \	 #set the database user
-		--dbpass=$SQL_PASSWORD \ #set the database user passwd
-		--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	echo "wp-config.php already exists baby"
+else
+	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
 
-wp core install --title=Inception \
+wp core install --allow-root --title=Inception \
 		--admin_user=$ADMIN_USER \
-		--admin_password=$ADMIN_PASSWORD
+		--admin_password=$ADMIN_PASSWORD \
+		--path='/var/www/wordpress'
 
-wp user_create $USER_NAME \
-		--user_pass=$USER_PASSWORD
+wp user create --allow-root $USER_NAME \
+		--user_pass=$USER_PASSWORD \
+		--path='/var/www/wordpress'
