4bdc2dfd43613e86837107b04c17ae45ea1f6f1c
louisea <louiseamelie2@gmail.com>
2023-01-06 18:09:51 +0100

    toutarefairewsh

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -3,18 +3,26 @@
 sleep 10 
 	# on veut etre sur que MariaDB c'est lance correctement avant de commencer la config WP
 
+# if [ ! -f "/var/www/html/index.html" ];
 if [ -f /var/www/wordpress/wp-config.php ];
 then
 	echo "wp-config.php already exists baby"
 else
-	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+	# mv /tmp/index.html /var/www/html/index.html
+
+	wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path=/var/www/wordpress --allow-root
 fi
 
-wp core install --allow-root --title=Inception \
-		--admin_user=$ADMIN_USER \
-		--admin_password=$ADMIN_PASSWORD \
-		--path='/var/www/wordpress'
+	wp core install --url=lmarecha.42.fr/wordpress \
+			--title=Inception \
+			--admin_user=$ADMIN_USER \
+			--admin_password=$ADMIN_PASSWORD \
+			--admin_email==$ADMIN_EMAIL \
+			--path=/var/www/wordpress \
+			--allow-root
 
-wp user create --allow-root $USER_NAME \
-		--user_pass=$USER_PASSWORD \
-		--path='/var/www/wordpress'
+	wp user create $USER_NAME $USER_EMAIL \
+			--user_pass=$USER_PASSWORD \
+			--path=/var/www/wordpress \
+		--allow-root
