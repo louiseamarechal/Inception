701b6b141fbb6ef3b602015cf79adaeabae8643e
louisea <louiseamelie2@gmail.com>
2023-01-10 15:18:21 +0100

    Mariadb is working properly

--- srcs/requirements/mariadb/conf/configure.sh
+++ srcs/requirements/mariadb/conf/configure.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 service mysql start
 
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 
-mysql -e "CREATE USER IF NOT EXIST '${SQL_USER}'@localhost IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@localhost IDENTIFIED BY '${SQL_PASSWORD}';"
 
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-mysql -e "FLUSH PRIVILEGES;"
+# mysql -e "FLUSH PRIVILEGES;"
 
-# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-# exec mysqld_safe
+# exit
+
+exec mysqld_safe
