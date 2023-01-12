37ed104328562d0ee38d4362f97d47d2a5b30e19
louisea <louiseamelie2@gmail.com>
2023-01-12 13:35:14 +0100

    updated wordpress config file to add user creation in my if statement

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -8,25 +8,25 @@ then
 	echo "wp-config.php already exists baby"
 else
 	wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path=/var/www/wordpress --allow-root
+
+	wp core install --url=lmarecha.42.fr/wordpress \
+			--title=Inception \
+			--admin_user=$ADMIN_USER \
+			--admin_password=$ADMIN_PASSWORD \
+			--admin_email==$ADMIN_EMAIL \
+			--path=/var/www/wordpress \
+			--allow-root
+
+	wp user create $USER_NAME $USER_EMAIL \
+			--user_pass=$USER_PASSWORD \
+			--path=/var/www/wordpress \
+		--allow-root
 fi
 
 if [ ! -f "/var/www/html/index.html" ];
 then
 	mv /tmp/index.html /var/www/html/index.html
 fi
-
-wp core install --url=lmarecha.42.fr/wordpress \
-		--title=Inception \
-		--admin_user=$ADMIN_USER \
-		--admin_password=$ADMIN_PASSWORD \
-		--admin_email==$ADMIN_EMAIL \
-		--path=/var/www/wordpress \
-		--allow-root
-
-wp user create $USER_NAME $USER_EMAIL \
-		--user_pass=$USER_PASSWORD \
-		--path=/var/www/wordpress \
-	--allow-root
 
 if [ ! -f "/run/php" ]
 then
